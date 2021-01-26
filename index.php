4f656dc16bcd49cafd3ad2e54ab1898f1c3d6f03
Viktory <blacklastday@gmail.com@git config --global user.email blacklastday@gmail.comgit config --global user.email blacklastday@gmail.com@>
2021-01-26 20:02:46 +0300

    Первый коммит

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE htm1>
+<html>
+	<head>
+		<title> Моя страничка </title>
+		<link rel="stylesheet" href="mycss.css">
+		<link rel="shortcut icon" href="favicon.ico">
+	</head>
+	<body>
+		<div class="mydiv1">
+			<?php
+				echo "<h1>Основания трапеции равны 10 и 6, высота равна 4, площадь данной трапеции равна  ",((10+6)/2) * 4;"</h1>"
+			?>
+		</div>
+	</body>
+</html>
